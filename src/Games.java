d7d3aadc808b26e4efdde83752c9eac54e61fc80
skr98 <skr9808@gmail.com>
2025-09-21 15:24:40 +0530

    added a new class games

---
+++ src/Games.java
@@ -0,0 +1,23 @@
+/**
+ * About Football
+ * About Cricket
+ */
+public class Games {
+    public static void main (String[] args) {
+        String gameName="football";
+        System.out.println(gameName);
+        int numberOfPlayers=20;
+        System.out.println(numberOfPlayers);
+        float playerRemunarteion=30.38f;
+        System.out.println(playerRemunarteion);
+        double playerRemunarteion19 =60.999;
+        System.out.println(playerRemunarteion19);
+        char playerName='r';
+        System.out.println(playerName);
+        boolean isafootbalGame=true;
+        System.out.println(isafootbalGame);
+
+        // singleline coment
+        /*multiplie line commt*/
+    }
+}
