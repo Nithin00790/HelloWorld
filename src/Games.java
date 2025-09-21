cf5d1c161d22dcd78a6d109ac45106255c996bfc
skr98 <skr9808@gmail.com>
2025-09-21 15:38:29 +0530

    added print statemnts

--- src/Games.java
+++ src/Games.java
@@ -5,13 +5,13 @@
 public class Games {
     public static void main (String[] args) {
         String gameName="football";
-        System.out.println(gameName);
+        System.out.println("game name is "+gameName);
         int numberOfPlayers=20;
-        System.out.println(numberOfPlayers);
+        System.out.println(numberOfPlayers+10);
         float playerRemunarteion=30.38f;
-        System.out.println(playerRemunarteion);
+        System.out.println(playerRemunarteion+9.5);
         double playerRemunarteion19 =60.999;
-        System.out.println(playerRemunarteion19);
+        System.out.println(playerRemunarteion19+3.55);
         char playerName='r';
         System.out.println(playerName);
         boolean isafootbalGame=true;
