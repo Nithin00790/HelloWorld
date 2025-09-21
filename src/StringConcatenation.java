232930f3bbf7c2a86ac5c759cec0ab91d37ded22
skr98 <skr9808@gmail.com>
2025-09-21 15:56:54 +0530

    StringConcatenation

---
+++ src/StringConcatenation.java
@@ -0,0 +1,9 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String firstName= "Nithin";
+        String lastName="Ronda";
+        String fullName= firstName+lastName;
+        System.out.println(fullName);
+
+    }
+}
